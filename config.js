38c251eb38ffc4ad6112d257cfddb67ce38d0d21
rob921533-dev <rob921533@gmail.com>
2026-09-24 23:11:33 +0300

    Update Supabase anon key in config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@
    ============================================================ */
 window.GOODBLOX_CONFIG = {
   SUPABASE_URL: "https://0ec90b57d6e95fcbda19832f.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJib2x0IiwicmVmIjoiMGVjOTBiNTdkNmU5NWZjYmRhMTk4MzJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4ODE1NzQsImV4cCI6MTc1ODg4MTU3NH0.9I8-U0x86Ak8t2DGaIk0HfvTSLsAyzdnz-Nw00mMkKw",
+  SUPABASE_ANON_KEY: "sb_publishable_9qWjySjK_apijQQn_UxrSQ_hwxGVj8V",
 
   APP_NAME: "GoodBlox",
 
